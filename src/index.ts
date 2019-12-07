288d84694f7c7b266aeef4f21f190843e9d9d7c5
gyuha <nicegyuha@gmail.com>
2019-12-07 12:42:40 +0900

    update get ip address

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as express from 'express';
 import * as cors from 'cors';
+import * as requestIp from 'request-ip';
 
 const app = express();
 app.use(cors());
+app.use(requestIp.mw())
 
-app.get('/', function (req, res) {
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+app.get('/', function (req: any, res) {
+  const ip = req.clientIp;
   res.json({ip});
 });
 
